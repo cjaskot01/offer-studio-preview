d29ee3522b7699e1f19028d6e1b01efab0509452
Chris Jaskot <cjaskot@onitdigital.com>
2026-08-11 11:43:41 -0400

    Add 'Paste from ML deck' tokenizer: auto-map data placeholders to variables, prompt editorial choices, hard-flag fill-ins (guardrail vs unresolved brackets)

--- assets/toggleswitch-BcunIcEp.js
+++ assets/toggleswitch-BcunIcEp.js
@@ -1,4 +1,4 @@
-import{Ft as e,Kt as t,Qt as n,Rt as r,Yt as i,f as a,lt as o}from"./_plugin-vue_export-helper-Dccn7nnx.js";import{l as s}from"./select-C5yOK4Sj.js";var c=a.extend({name:`toggleswitch`,style:`
+import{Ft as e,Kt as t,Qt as n,Rt as r,Yt as i,f as a,lt as o}from"./_plugin-vue_export-helper-Dccn7nnx.js";import{l as s}from"./select-DjF8ZWZJ.js";var c=a.extend({name:`toggleswitch`,style:`
     .p-toggleswitch {
         display: inline-block;
         width: dt('toggleswitch.width');
